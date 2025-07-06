f526f7bef163a65a37f64f0d4edafdc5bf59aaea
tshanli <tshanli@outlook.com>
2025-07-06 19:00:41 +0200

    Add new cask 'kazumi'

---
+++ Casks/kazumi.rb
@@ -0,0 +1,25 @@
+cask "kazumi" do
+  version "1.7.4"
+  sha256 "13a8c8de746b9aeedbd4fdc58a23eb867e90deafdf7569e6687c142e4f8c8e51"
+
+  url "https://github.com/Predidit/Kazumi/releases/download/#{version}/Kazumi_macos_#{version}.dmg"
+  name "Kazumi"
+  desc "Anime streaming application with custom rules and real-time super-resolution"
+  homepage "https://github.com/Predidit/Kazumi"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Kazumi.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kazumi",
+    "~/Library/Caches/Kazumi",
+    "~/Library/Logs/Kazumi",
+    "~/Library/Preferences/com.predidit.kazumi.plist",
+    "~/Library/Saved Application State/com.predidit.kazumi.savedState",
+    "~/Library/WebKit/Kazumi",
+  ]
+end
