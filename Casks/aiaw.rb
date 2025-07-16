c43af2ef0c8d6fa1369dee2db5980b531863885d
GitHub Action <action@github.com>
2025-07-16 02:52:37 +0000

    Update AIaW to version 1.7.6

--- Casks/aiaw.rb
+++ Casks/aiaw.rb
@@ -1,7 +1,7 @@
 cask "aiaw" do
-  version "1.7.5"
-  sha256 arm:   "746c30adb30a984993e3b43ccc4988f053aab01de807b46c836cec247285e444",
-         intel: "14096d93d26e2f32230e1a17cd56b3fe61c82114e33dcb55ec9c3b10a1137922"
+  version "1.7.6"
+  sha256 arm:   "ddd09e0681eaf982edd4aaa2a89f31bcf150b6dfc505de0060b2e2976578199c",
+         intel: "2785b4b6e61c7dcf48d9188f74a8401f42c7d200e64ad5cb310aa4433c63cf78"
 
   url "https://github.com/NitroRCr/AIaW/releases/download/v#{version}/AI.as.Workspace_#{version}_#{Hardware::CPU.intel? ? "x64" : "aarch64"}.dmg"
   name "AI as Workspace"
