a1ca2e46bed328b5ea31808e27d523ee3b32d78a
GitHub Action <action@github.com>
2026-02-12 03:39:51 +0000

    Update SourceGit develop to commit e1aaee2

--- Formula/sourcegit-develop.rb
+++ Formula/sourcegit-develop.rb
@@ -2,14 +2,14 @@ class SourcegitDevelop < Formula
   desc 'Open-source Git GUI client (develop branch build)'
   homepage 'https://github.com/sourcegit-scm/sourcegit'
   url 'https://github.com/sourcegit-scm/sourcegit/archive/refs/heads/develop.zip'
-  version '20260211-45a3936'
+  version '20260211-e1aaee2'
 
   depends_on 'dotnet'
   depends_on 'openssl@3'
 
   def install
     # Use static version string for Homebrew build - updated by GitHub Action
-    commit_hash = '45a3936'
+    commit_hash = 'e1aaee2'
 
     # Build and publish
     system 'dotnet', 'build', '-c', 'Release'
