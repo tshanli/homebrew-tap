539d6d929b3b729b864767fa1e7858626fc3b1df
GitHub Action <action@github.com>
2026-08-22 02:12:36 +0000

    Update Kazumi to version 2.2.9

--- Casks/kazumi.rb
+++ Casks/kazumi.rb
@@ -1,6 +1,6 @@
 cask "kazumi" do
-  version "2.2.8"
-  sha256 "f3c387a36f7320799ff10f5a08bc7b6a368de5e9aa804a6b6457507669d140dc"
+  version "2.2.9"
+  sha256 "79611b14ddbec5b715568eb1cb61ba23bdc18fc4011486340ed782e7ac016318"
 
   url "https://github.com/Predidit/Kazumi/releases/download/#{version}/Kazumi_macos_#{version}.dmg"
   name "Kazumi"
